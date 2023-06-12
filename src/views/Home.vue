498c1a09b8955a74951684db758051d4d06e58d0
Preston Roesslet <proesslet@gmail.com>
2023-06-11 20:41:44 -0500

    user fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
         />
       </div>
 
-      <div class="row w-50 m-auto align-center px-0 mb-1">
+      <div class="row w-75 m-auto align-center px-0 mb-1">
         <div class="col-12 px-0">
           <button class="btn btn-primary float-end" @click="showAddForm">
             Add Task
